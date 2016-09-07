4b821f203ad10904f439ac274935c1b95df258de
Balazs Meszaros <mesz@zhaw.ch>
2016-09-07 15:30:10 +0200

    Python path has to be set as well within Zeppelin

--- sm/managers/data/zeppelin-env.sh
+++ sm/managers/data/zeppelin-env.sh
@@ -6,3 +6,5 @@ export SPARK_HOME=/usr/lib/spark/spark
 export HADOOP_HOME=/usr/lib/hadoop/hadoop
 export HADOOP_CONF_DIR=/etc/hadoop
 export ZEPPELIN_MEM=-Xmx4g
+
+export PYTHONPATH=/usr/bin/python3
