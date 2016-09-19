340b3cfbb283a99ab53de26b17a4218f6659c9e9
Balazs Meszaros <mesz@zhaw.ch>
2016-09-19 16:42:34 +0200

    mechanism for setting the variables integrated into cluster framework

--- sm/managers/clusterframework.py
+++ sm/managers/clusterframework.py
@@ -3,6 +3,7 @@ from framework import Framework
 class ClusterFramework(Framework):
     def __init__(self, deployClass, attributes):
         super(ClusterFramework,self).__init__(deployClass, attributes)
+        self.variables = {"master_name": "mymaster", "slave_name": "myslave", "slave_count": 1}
 
     def get_bash(self):
         return ''
@@ -13,3 +14,5 @@ class ClusterFramework(Framework):
     def get_dependencies(self):
         return {}
 
+    def set_variables(self, varDict):
+        self.variables = varDict
