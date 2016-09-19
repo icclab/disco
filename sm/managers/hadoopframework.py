b67a70ba9ae0a1552c1d2be3831e5a032b46b2df
Balazs Meszaros <mesz@zhaw.ch>
2016-09-19 16:44:36 +0200

    creation of slaves / masters files in Hadoop config directory fixed

--- sm/managers/hadoopframework.py
+++ sm/managers/hadoopframework.py
@@ -1,9 +1,11 @@
+from sm.log import LOG
 from framework import Framework
 
 class HadoopFramework(Framework):
     def __init__(self, deployClass, attributes):
         super(HadoopFramework,self).__init__(deployClass, attributes)
-        self.dependencies = {}
+        self.dependencies = {"jdk": {},"cluster":{"master_name":{},"slave_count":{},"slave_name":{}}}
+        self.variables = {"hadoop": {"slaveonmaster": True}}
 
     def get_bash(self):
         returnValue = self.deployClass.getFileContent("hadoopbash.sh")
@@ -22,12 +24,27 @@ class HadoopFramework(Framework):
         }
         for key, value in replaceDict.iteritems():
             returnValue = returnValue.replace(key, value)
-
+        returnValue = returnValue.replace( '$slavesfile$', self.get_slavesfile())
+        masterreplacement = ''
+        if self.variables["hadoop"]["slaveonmaster"]:
+            masterreplacement = self.dependencies['cluster']['master_name']+"\n"
+        returnValue = returnValue.replace( '$masternodeasslave$',masterreplacement)
+        returnValue = returnValue.replace( '$masternode$', self.dependencies['cluster']['master_name'])
         return returnValue
 
+    def get_slavesfile(self):
+        slavesfile = ''
+        try:
+            slavecount = int(self.dependencies['cluster']['slave_count'])
+            slavename = self.dependencies['cluster']['slave_name']
+            slavesfile = ''
+            for i in xrange(1, slavecount+1):
+                slavesfile += slavename+str(i)+"\n"
+        except:
+            LOG.error("either slave count or slave name not in dependencies")
+        return slavesfile
     def get_name(self):
         return "hadoop"
 
     def get_dependencies(self):
-        self.dependencies = {"jdk": {}}
         return self.dependencies
