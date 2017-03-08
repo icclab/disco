ae12ecff32fbffcf2f994796b3288ae6d437d351
Balazs Meszaros <mesz@zhaw.ch>
2017-03-08 16:08:49 +0100

    - updated license / copyright information - deleted not needed files

--- sm/managers/data/hadoop/fw1.py
+++
@@ -1,5 +0,0 @@
-def helloworld(context,myarg):
-    return 10*str(myarg)
-
-def mysecondfunction(context):
-    return 23*"foo"
--- sm/managers/data/shell/shellfunctions.py
+++ sm/managers/data/shell/shellfunctions.py
@@ -1,4 +1,19 @@
-from lxml import etree
+# Copyright (c) 2017. Zuercher Hochschule fuer Angewandte Wissenschaften
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# Author: Balazs Meszaros
 
 #TODO: make this method more generic: multiple shell tags should be handled just like shell tags at the very beginning of the string
 def indentshell(context,allcontent):
